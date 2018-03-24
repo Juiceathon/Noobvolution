f0ee6c991f64e1691a06d5a8ec258e3c140461cf
Derrick Cross <decross1@me.com>
2018-03-24 14:30:13 -0700

    Initial Loading Page with Router

--- src/App.js
+++ src/App.js
@@ -1,90 +1,36 @@
 import React, { Component } from 'react';
-import FixedMenu from './components/FixedMenu.jsx';
-import MiddleHomePageSegment from './components/MiddleHomePageSegment.jsx';
-import Footer from './components/Footer.jsx';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
 import {
-  Button,
-  Container,
-  Header,
-  Icon,
-  Menu,
-  Segment,
-  Visibility,
-} from 'semantic-ui-react';
+  BrowserRouter as Router, 
+  Switch,
+  Route, 
+  Link
+} from 'react-router-dom';
+
+import Footer from './components/Footer.jsx';
+import FixedMenu from './components/FixedMenu.jsx';
+
+import HomePage from './components/home/HomePage.jsx';
+
+import './App.css';
 
 class App extends Component {
  constructor() {
     super()
     this.state = {}
-
-    this.hideFixedMenu = this.hideFixedMenu.bind(this);
-    this.showFixedMenu = this.showFixedMenu.bind(this);
-  }
-
-  hideFixedMenu() {
-    this.setState({ visible: false })
-  }
-
-  showFixedMenu() {
-    this.setState({ visible: true })
-  }
+ }
 
   render() {
-    const { visible } = this.state
-
     return (
-      <div>
-        {visible ? <FixedMenu /> : null}
+    <div>
+      <FixedMenu />
+      <Router>
+        <Switch> 
+          <Route exact path='/' component={HomePage} />
 
-        <Visibility
-          onBottomPassed={this.showFixedMenu}
-          onBottomVisible={this.hideFixedMenu}
-          once={false}
-        >
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
-            vertical
-          >
-            <Container>
-              <Menu inverted pointing secondary size='large'>
-                <Menu.Item as='a' active>Home</Menu.Item>
-                <Menu.Item as='a'>About</Menu.Item>
-                <Menu.Item as='a'>Coaches</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted>Log in</Button>
-                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-                </Menu.Item>
-              </Menu>
-            </Container>
-
-            <Container text>
-              <Header
-                as='h1'
-                content='Noobvolution'
-                inverted
-                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
-              />
-              <Header
-                as='h2'
-                content="Receive Coaching from the Top Gaming Pro's"
-                inverted
-                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
-              />
-              <Button primary size='huge'>
-                Get Started
-                <Icon name='right arrow' />
-              </Button>
-            </Container>
-          </Segment>
-        </Visibility>
-
-        <MiddleHomePageSegment />
-        <Footer />
-      </div>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
     )
   }
 }
