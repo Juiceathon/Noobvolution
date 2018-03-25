30e719c29544817e1dff498960dfc6f1a41714a0
Jackie Jou <jakeic46@gmail.com>
2018-03-24 22:09:01 -0700

    videoChat component bork

---
+++ src/components/video/VideoChat.jsx
@@ -0,0 +1,203 @@
+import React, { Component } from 'react';
+import Spinner from 'react-spinner';
+import classNames from 'classnames';
+
+import AccCore from 'opentok-accelerator-core';
+import 'opentok-solutions-css';
+
+// import config from '../../lambda/config.js';
+import config from './config.json';
+import './VideoChat.css';
+
+import {createSessionId, createToken} from '../../lambda/tokbox.js';
+
+let otCore;
+const otCoreOptions = {
+  credentials: {
+    apiKey: config.apiKey,
+    sessionId: config.sessionId,
+    token: config.token,
+  },
+  // A container can either be a query selector or an HTML Element
+  streamContainers(pubSub, type, data, stream) {
+    return {
+      publisher: {
+        camera: '#cameraPublisherContainer',
+        screen: '#screenPublisherContainer',
+      },
+      subscriber: {
+        camera: '#cameraSubscriberContainer',
+        screen: '#screenSubscriberContainer',
+      },
+    }[pubSub][type];
+  },
+  controlsContainer: '#controls',
+  packages: ['textChat', 'screenSharing', 'annotation'],
+  communication: {
+    callProperites: null, // Using default
+  },
+  textChat: {
+    name: 'Username', // FROM LOGIN
+    waitingMessage: 'Messages will be delivered when other users arrive',
+    container: '#chat',
+    alwaysOpen: true,
+  },
+  screenSharing: {
+    extensionID: 'nbklomglchjeghljjhjahgjobppkfpfm',
+    annotation: true,
+    externalWindow: false,
+    dev: true,
+    screenProperties: {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%',
+      showControls: false,
+      style: {
+        buttonDisplayMode: 'off',
+      },
+      videoSource: 'window',
+      fitMode: 'contain' // Using default
+    },
+  },
+  annotation: {
+    absoluteParent: {
+      publisher: '.App-video-container',
+      subscriber: '.App-video-container'
+    }
+  },
+};
+
+/**
+ * Build classes for container elements based on state
+ * @param {Object} state
+ */
+const containerClasses = (state) => {
+  const { active, meta, localAudioEnabled, localVideoEnabled } = state;
+  const sharingScreen = meta ? !!meta.publisher.screen : false;
+  const viewingSharedScreen = meta ? meta.subscriber.screen : false;
+  const activeCameraSubscribers = meta ? meta.subscriber.camera : 0;
+  const activeCameraSubscribersGt2 = activeCameraSubscribers > 2;
+  const activeCameraSubscribersOdd = activeCameraSubscribers % 2;
+  const screenshareActive = viewingSharedScreen || sharingScreen;
+  return {
+    controlClass: classNames('App-control-container', { hidden: !active }),
+    localAudioClass: classNames('ots-video-control circle audio', { hidden: !active, muted: !localAudioEnabled }),
+    localVideoClass: classNames('ots-video-control circle video', { hidden: !active, muted: !localVideoEnabled }),
+    localCallClass: classNames('ots-video-control circle end-call', { hidden: !active }),
+    cameraPublisherClass: classNames('video-container', { hidden: !active, small: !!activeCameraSubscribers || screenshareActive, left: screenshareActive }),
+    screenPublisherClass: classNames('video-container', { hidden: !active || !sharingScreen }),
+    cameraSubscriberClass: classNames('video-container', { hidden: !active || !activeCameraSubscribers },
+      { 'active-gt2': activeCameraSubscribersGt2 && !screenshareActive },
+      { 'active-odd': activeCameraSubscribersOdd && !screenshareActive },
+      { small: screenshareActive }
+    ),
+    screenSubscriberClass: classNames('video-container', { hidden: !viewingSharedScreen || !active }),
+  };
+};
+
+const connectingMask = () =>
+  <div className="App-mask">
+    <Spinner />
+    <div className="message with-spinner">Connecting</div>
+  </div>;
+
+const startCallMask = start =>
+  <div className="App-mask">
+    <button className="message button clickable" onClick={start}>Click to Video Chat</button>
+  </div>;
+
+class VideoChat extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      connected: false,
+      active: false,
+      publishers: null,
+      subscribers: null,
+      meta: null,
+      localAudioEnabled: true,
+      localVideoEnabled: true,
+    };
+    this.startCall = this.startCall.bind(this);
+    this.endCall = this.endCall.bind(this);
+    this.toggleLocalAudio = this.toggleLocalAudio.bind(this);
+    this.toggleLocalVideo = this.toggleLocalVideo.bind(this);
+  }
+
+  componentDidMount() {
+    otCore = new AccCore(otCoreOptions);
+    otCore.connect().then(() => this.setState({ connected: true }));
+    const events = [
+      'subscribeToCamera',
+      'unsubscribeFromCamera',
+      'subscribeToScreen',
+      'unsubscribeFromScreen',
+      'startScreenShare',
+      'endScreenShare',
+    ];
+
+    events.forEach(event => otCore.on(event, ({ publishers, subscribers, meta }) => {
+      this.setState({ publishers, subscribers, meta });
+    }));
+  }
+
+  startCall() {
+    otCore.startCall()
+      .then(({ publishers, subscribers, meta }) => {
+        this.setState({ publishers, subscribers, meta, active: true });
+      }).catch(error => console.log(error));
+  }
+
+  endCall() {
+    otCore.endCall();
+    this.setState({ active: false });
+  }
+
+  toggleLocalAudio() {
+    otCore.toggleLocalAudio(!this.state.localAudioEnabled);
+    this.setState({ localAudioEnabled: !this.state.localAudioEnabled });
+  }
+
+  toggleLocalVideo() {
+    otCore.toggleLocalVideo(!this.state.localVideoEnabled);
+    this.setState({ localVideoEnabled: !this.state.localVideoEnabled });
+  }
+
+  render() {
+    const { connected, active } = this.state;
+    const {
+      localAudioClass,
+      localVideoClass,
+      localCallClass,
+      controlClass,
+      cameraPublisherClass,
+      screenPublisherClass,
+      cameraSubscriberClass,
+      screenSubscriberClass,
+    } = containerClasses(this.state);
+
+    return (
+      <div className="App">
+        <div className="App-main">
+          <div className="App-video-container">
+            { !connected && connectingMask() }
+            { connected && !active && startCallMask(this.startCall)}
+            <div id="cameraPublisherContainer" className={cameraPublisherClass} />
+            <div id="screenPublisherContainer" className={screenPublisherClass} />
+            <div id="cameraSubscriberContainer" className={cameraSubscriberClass} />
+            <div id="screenSubscriberContainer" className={screenSubscriberClass} />
+          </div>
+          <div id="controls" className={controlClass}>
+            <div className={localAudioClass} onClick={this.toggleLocalAudio} />
+            <div className={localVideoClass} onClick={this.toggleLocalVideo} />
+            <div className={localCallClass} onClick={this.endCall} />
+          </div>
+          <div id="chat" className="App-chat-container" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VideoChat;
--- src/lambda/tokbox.js
+++ src/lambda/tokbox.js
@@ -1,22 +1,31 @@
 var OpenTok = require('opentok');
 var {API_KEY, API_SECRET} = require('../../utils/config');
 var opentok = new OpenTok(API_KEY, API_SECRET);
-var sessionId;
-opentok.createSession({mediaMode:"routed"}, function(error, session) {
-  if (error) {
-    console.log("Error creating session:", error)
-  } else {
-    sessionId = session.sessionId;
-    console.log("Session ID: " + sessionId);
-    var token = opentok.generateToken(sessionId, {role: 'publisher', data: 'userid=1'});
-    console.log('TOKEN', token);
-  }
-});
 
-export function tokBox (event, context, callback) {
-  console.log(event);
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({sessionId: "Hello, World!", token: 'AAAAAA'})
+var createSessionId = () => {
+  return new Promise ((resolve, reject) => {
+    opentok.createSession({mediaMode:"routed"}, function(error, session) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(session.sessionId);
+      }
+    });
   });
 }
+
+var createToken = (sessionId) => {
+  return opentok.generateToken(sessionId, {role: 'publisher'});
+};
+
+module.exports = {
+  createSessionId,
+  createToken
+}
+// export function tokBox (event, context, callback) {
+//   console.log(event);
+//   callback(null, {
+//     statusCode: 200,
+//     body: JSON.stringify({sessionId: "Hello, World!", token: 'AAAAAA'})
+//   });
+// }
